32de32eb8d57aff67e0d640d61086ebe902e06e6
Eddaoust <edmond.daoust@gmail.com>
2018-10-08 16:59:15 +0200

    Modification du service

--- src/Service/TwigGlobalCategory.php
+++ src/Service/TwigGlobalCategory.php
@@ -10,15 +10,37 @@ namespace App\Service;
 
 
 use App\Entity\Category;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TwigGlobalCategory
 {
-    public function getCategories(EntityManager $entityManager)
+    /**
+     * Service permettant d'accéder aux catégories de montre globalement
+     */
+    private $categories;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
         $categoriesRepo = $entityManager->getRepository(Category::class);
         $categories = $categoriesRepo->findAll();
 
-        return $categories;
+        $this->setCategories($categories);
     }
+
+    /**
+     * @return mixed
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * @param mixed $categories
+     */
+    public function setCategories($categories): void
+    {
+        $this->categories = $categories;
+    }
+
 }
