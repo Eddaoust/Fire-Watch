4f570d420c896be5125b029ee1bca018054e6563
Eddaoust <edmond.daoust@gmail.com>
2018-10-11 14:42:11 +0200

    Ajout tableaux

--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +14,17 @@ class ProductAdminController extends AbstractController
      */
     public function homeAdmin()
     {
-        return $this->render('product_admin/home_admin.html.twig');
+        $repo = $this->getDoctrine()
+            ->getRepository(Product::class);
+        $products = $repo->findAll();
+
+        $repo = $this->getDoctrine()
+            ->getRepository(User::class);
+        $users = $repo->findAll();
+
+        return $this->render('product_admin/home_admin.html.twig', [
+            'products' => $products,
+            'users' => $users
+        ]);
     }
 }
